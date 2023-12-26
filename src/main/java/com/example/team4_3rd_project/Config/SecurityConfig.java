122e0e83a22a21b7281af254783567218a111b07
강정현 <life.us0410@gmail.com>
2023-12-26 12:57:49 +0900

    cors 설정 완료1

--- src/main/java/com/example/team4_3rd_project/Config/SecurityConfig.java
+++ src/main/java/com/example/team4_3rd_project/Config/SecurityConfig.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
