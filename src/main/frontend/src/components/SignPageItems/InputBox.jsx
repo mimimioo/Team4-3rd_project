68787e901904a131f6d8d83f9ac7b7277d723753
강정현 <life.us0410@gmail.com>
2023-12-27 16:37:01 +0900

    로그인 페이지 뷰 4차 수정/기능 작업중

--- src/main/frontend/src/components/SignPageItems/InputBox.jsx
+++ src/main/frontend/src/components/SignPageItems/InputBox.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 
 const InputBox = (props) => {
+    const inputBasicStyle = {
+        border: "1px solid #aaa",
+        boxShadow: "none"
+    };
+    const inputBasicTextStyle = {
+        left: "0",
+        top: "50%",
+        transform: "translateY(-50%)",
+        color: "#aaa",
+        fontSize: "14px"
+    };
+    const inputStyle = {
+        border: "1px solid rgba(0, 0, 0, 0)",
+        boxShadow: "0 0 3px 2px dodgerblue"
+    };
+    const inputTextStyle = {
+        left: "5px",
+        top: "-15%",
+        transform: "translateY(0)",
+        color: "dodgerblue",
+        fontSize: "10px"
+    };
+    const inputBlurStyle = {
+        border: "1px solid #aaa",
+        boxShadow: "0 0 3px 2px #aaa"
+    };
+    const inputBlurTextStyle = {
+        left: "5px",
+        top: "-15%",
+        transform: "translateY(0)",
+        color: "#aaa",
+        fontSize: "10px"
+    };
+    const [inputDynamicStyle, setInputDynamicStyle] = useState(inputBasicStyle);
+    const [inputDynamicTextStyle, setInputDynamicTextStyle] = useState(inputBasicTextStyle);
+    const setInputData = (event) => {
+        if(event.type === 'focus' || event.type === 'change') {
+            if(event.target.value != null)
+                props.onDataFromInput(props.type, event.target.value);
+            setInputDynamicStyle(inputStyle);
+            setInputDynamicTextStyle(inputTextStyle);
+        } else {
+            if(event.target.value !== '') {
+                setInputDynamicStyle(inputBlurStyle);
+                setInputDynamicTextStyle(inputBlurTextStyle);
+            } else {
+                setInputDynamicStyle(inputBasicStyle);
+                setInputDynamicTextStyle(inputBasicTextStyle);
+            }
+        }
+    }
+
     return (
         <InputWrapper>
-            <Input />
-            <InputText>{props.message}</InputText>
+            <Input onChange={setInputData} onFocus={setInputData} onBlur={setInputData} style={inputDynamicStyle}/>
+            <InputText style={inputDynamicTextStyle}>{props.message}</InputText>
         </InputWrapper>
     );
 };
@@ -13,9 +65,8 @@ export default InputBox;
 
 const InputWrapper = styled.div`
     position: relative;
-    margin: 0 auto;
+    margin: 0 auto 20px;
     width: 70%;
-    margin-bottom: 20px;
     text-align: center;
     max-width: 350px;
 `
@@ -27,11 +78,6 @@ const Input = styled.input`
   max-width: 400px;
   padding: 10px;
   transition: 0.5s ease;
-
-  &:focus {
-    border: 1px solid rgba(0, 0, 0, 0);;
-    box-shadow: 0 0 3px 2px dodgerblue;
-  }
 `
 const InputText = styled.div`
     color: #aaa;
@@ -47,13 +93,4 @@ const InputText = styled.div`
     margin-left: 10px;
     transition: 0.5s ease;
     font-size: 14px;
-
-    ${Input}:focus ~ & {
-      left: 5px;
-      top: -15%;
-      transform: translateY(0);
-      color: dodgerblue;
-      font-size: 10px;
-      //box-shadow: 0 0 3px 1px dodgerblue;
-    }
 `
--- src/main/frontend/src/components/SignPageItems/SignBtn.jsx
+++ src/main/frontend/src/components/SignPageItems/SignBtn.jsx
@@ -3,16 +3,43 @@ import styled from "styled-components";
 import {Link} from "react-router-dom";
 
 const SignBtn = (props) => {
-    return (
-        <Btn to={props.to}>
-            {props.children}
-        </Btn>
-    );
+    if(props.to) {
+        return (
+            <PageLink to={props.to}>
+                {props.children}
+            </PageLink>
+        );
+    } else {
+        return (
+            <Btn>
+                {props.children}
+            </Btn>
+        );
+    }
 };
 
 export default SignBtn;
 
-const Btn = styled(Link)`
+const PageLink = styled(Link)`
+  width: 70%;
+  max-width: 350px;
+  margin-bottom: 10px;
+  text-align: center;
+  background-color: dodgerblue;
+  outline: none;
+  border: none;
+  color: #fff;
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 10px;
+  transition: 0.3s ease;
+  text-decoration: none;
+
+  &:active {
+    background-color: #197bd9; /* 버튼이 눌러진 상태일 때 배경 색상 변경 */
+  }
+`
+const Btn = styled.button`
   width: 70%;
   max-width: 350px;
   margin-bottom: 10px;
