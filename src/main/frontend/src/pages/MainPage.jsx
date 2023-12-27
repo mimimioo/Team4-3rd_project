2a851ac195a3ecd62d6918b22a7e66740e42f285
mimimioo <thd0071@naver.com>
2023-12-27 16:34:05 +0900

    중간 저장

--- src/main/frontend/src/pages/MainPage.jsx
+++ src/main/frontend/src/pages/MainPage.jsx
@@ -2,6 +2,22 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import BackgroundVideo from '../components/BackgroundVideo';
+import Nav from '../components/Nav/Nav';
+
+const Main = () => {
+  return (
+    <StyledContainer>
+      <BackgroundVideo />
+      <Nav/>  
+    </StyledContainer>
+  );
+};
+
+export default Main;
+
+
+
 
 const StyledContainer = styled.div`
   position: relative;
@@ -9,73 +25,3 @@ const StyledContainer = styled.div`
   height: 100vh;
   overflow: hidden;
 `;
-
-const VideoBackground = styled.video`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
-
-const StyledNav = styled.nav`
-  position: relative;
-  z-index: 1;
-  background-color: transparent;
-  padding: 10px;
-  text-align: center;
-  display: flex;
-  justify-content: space-between; /* 좌우 간격을 최대로 설정 */
-  align-items: center;
-`;
-
-const StyledText = styled.div`
-  font-size: 24px;
-  margin-bottom: 10px;
-  font-family: 'OA';
-  color: white;
-  margin-right: 100px;
-`;
-
-const StyledLinkContainer = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const StyledLink = styled.a`
-  color: white;
-  margin-right: 20px;
-  text-decoration: none;
-  font-family: 'OA';
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const Main = () => {
-  return (
-    <StyledContainer>
-      <VideoBackground autoPlay loop muted>
-        <source src={process.env.PUBLIC_URL + '/videos/fish.mp4'} type="video/mp4" />
-        Your browser does not support the video tag.
-      </VideoBackground>
-      <StyledNav>
-        <StyledLinkContainer>
-          <StyledLink href="#">선상예약</StyledLink>
-          <StyledLink href="#">커뮤니티</StyledLink>
-          <StyledLink href="#">중고마켓</StyledLink>
-          <StyledLink href="#">공지사항</StyledLink>
-        </StyledLinkContainer>
-        <StyledText>낚시의민족낚았소</StyledText>
-        <StyledLinkContainer>
-          <StyledLink href="#">낚시터찾기</StyledLink>
-          <StyledLink href="#">로그인</StyledLink>
-        </StyledLinkContainer>
-      </StyledNav>
-    </StyledContainer>
-  );
-};
-
-export default Main;
---
+++ src/main/frontend/src/pages/UsedMarket.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UsedMarket = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UsedMarket;
