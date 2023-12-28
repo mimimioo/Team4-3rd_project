33a47db88e512752ae556122705e493baab47dea
강정현 <life.us0410@gmail.com>
2023-12-28 13:08:10 +0900

    로그인 페이지 뷰 4차 수정/기능 작업중

--- src/main/frontend/src/components/SignPageItems/SignBtn.jsx
+++ src/main/frontend/src/components/SignPageItems/SignBtn.jsx
@@ -25,18 +25,19 @@ const PageLink = styled(Link)`
   max-width: 350px;
   margin-bottom: 10px;
   text-align: center;
-  background-color: dodgerblue;
+  background-color: #fff;
   outline: none;
   border: none;
-  color: #fff;
+  color: dodgerblue;
   font-weight: bold;
-  padding: 10px;
+  font-size: 14px;
+  padding: 10px 0;
   border-radius: 10px;
   transition: 0.3s ease;
   text-decoration: none;
 
   &:active {
-    background-color: #197bd9; /* 버튼이 눌러진 상태일 때 배경 색상 변경 */
+    background-color: #dedede; /* 버튼이 눌러진 상태일 때 배경 색상 변경 */
   }
 `
 const Btn = styled.button`
@@ -49,6 +50,7 @@ const Btn = styled.button`
   border: none;
   color: #fff;
   font-weight: bold;
+  font-size: 14px;
   padding: 10px;
   border-radius: 10px;
   transition: 0.3s ease;
